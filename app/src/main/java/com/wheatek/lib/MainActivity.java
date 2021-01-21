7c9f0e1e2fae0b43c912a96b566a052c2450ffc5
shiju.wang <shiju.wang@wheatek.com>
2021-01-21 20:06:03 +0800

    add jpush

--- app/src/main/java/com/wheatek/lib/MainActivity.java
+++ app/src/main/java/com/wheatek/lib/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.wheatek.wheateklib.ImageUtil;
+
+import okhttp3.OkHttp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
